3a6e6bc66e174ffffce3687cc7f447018e85d659
OlgaSwan <137110816+OlgaSwan@users.noreply.github.com>
2023-11-28 19:01:52 +0300

    chore: eslint config

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {browser: true, es2020: true},
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'next',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  rules: {
+    'no-console': 'warn',
+    'quotes': ['off', 'single'],
+    'jsx-quotes': ['off', 'prefer-single'],
+    'indent': ['off', 2],
+    'max-len': ['off', {'code': 120}],
+    'comma-dangle': ['off', true],
+    'semi': ['off', false],
+  },
+}
