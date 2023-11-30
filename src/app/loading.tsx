00f3e90028a9e1deef1d85b5787ce9604df3a122
OlgaSwan <137110816+OlgaSwan@users.noreply.github.com>
2023-11-30 19:06:15 +0300

    raw structure

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from './page.module.css'
+
+const Loading = () => {
+  return (
+    <main className={styles.main}>Loading...</main>
+  )
+}
+export default Loading
---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from './page.module.css'
+
+const NotFound = () => {
+  return (
+    <main className={styles.main}>NotFound</main>
+  )
+}
+export default NotFound
