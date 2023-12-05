5d4bb812feffc24a75427b1142b8ceafd0e4bf6b
parlyment <parlament20000@gmail.com>
2023-12-05 19:48:07 +0200

    fix tryGetFromLocalStorage

--- src/app/pet-store.ts
+++ src/app/pet-store.ts
@@ -11,14 +11,18 @@ const store = atom<Pet | undefined>(undefined)
 export const petStore = {
   store,
   tryGetFromLocalStorage: action(store, 'tryGetFromLocalStorage', (store) => {
-    const localPet = localStorage.getItem(KEY)
-    if (localPet) store.set(JSON.parse(localPet) as Pet)
+    try {
+      const localPet = localStorage.getItem(KEY)
+      if (localPet) store.set(JSON.parse(localPet) as Pet)
+    } catch (error) {
+      //ignored
+    }
   }),
-  createPet: action(store, 'createPet', ( store, pet: Pet ) => {
+  createPet: action(store, 'createPet', (store, pet: Pet) => {
     store.set(pet)
     localStorage.setItem('pet', JSON.stringify(pet))
   }),
-  reduceNeeds: action(store, 'reduceNeeds', ( store, field: Field ) => {
+  reduceNeeds: action(store, 'reduceNeeds', (store, field: Field) => {
     const pet = store.get()
     if (pet && pet[field] > 0) {
       const newValue = { ...pet, [field]: pet[field] - 1 }
