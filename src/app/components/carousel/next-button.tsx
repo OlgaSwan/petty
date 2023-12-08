724157b5e6d85bc1d092f464c8c539c1c0ebc6c1
OlgaSwan <137110816+OlgaSwan@users.noreply.github.com>
2023-12-08 03:11:22 +0300

    carousel nav btns hovering

--- src/app/components/carousel/next-button.tsx
+++ src/app/components/carousel/next-button.tsx
@@ -4,7 +4,7 @@ type Props = JSX.IntrinsicElements['svg'];
 
 function NextButton( props: Props ) {
   return (
-    <svg {...props} xmlns='http://www.w3.org/2000/svg' width='12' height='20' viewBox='0 0 12 20' fill='none'>
+    <svg {...props} xmlns='http://www.w3.org/2000/svg' width='12' height='20' viewBox='0 0 12 20' fill='transparent'>
       <path
         d='M11 9V8H10V7H8V5H6V3H4V1H2V0H0V3H2V5H4V7H6V9H8V11H6V13H4V15H2V17H0V20H2V19H4V17H6V16V15H8V14V13H10V12H11V11H12V10V9H11Z'
         fill='black'/>
--- src/app/components/carousel/prev-button.tsx
+++ src/app/components/carousel/prev-button.tsx
@@ -4,7 +4,7 @@ type Props = JSX.IntrinsicElements['svg'];
 
 function PrevButton( props: Props ) {
   return (
-    <svg {...props} xmlns='http://www.w3.org/2000/svg' width='12' height='20' viewBox='0 0 12 20' fill='none'>
+    <svg {...props} xmlns='http://www.w3.org/2000/svg' width='12' height='20' viewBox='0 0 12 20' fill='transparent'>
       <path
         d='M10 0V1H8V3H6V5H4V7H2V8H1V9H0V11H1V12H2V13H4V15H6V17H7H8V18V19H10V20H12V17H10V15H8V13H6V11H4V9H6V7H8V5H10V3H12V0H10Z'
         fill='black'/>
