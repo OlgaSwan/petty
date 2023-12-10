ca08f03ca6d8548adc7ec075172ffac6405374b5
OlgaSwan <137110816+OlgaSwan@users.noreply.github.com>
2023-12-10 05:33:19 +0300

    saving volume locally

--- src/app/(game-scope)/game/components/noti/noti.tsx
+++ src/app/(game-scope)/game/components/noti/noti.tsx
@@ -2,34 +2,37 @@
 
 import React, { useCallback, useEffect, useRef, useState } from 'react'
 
-import { petStore } from '@component/app/pet-store'
+import { Field, petStore } from '@component/app/pet-store'
 import { useStore } from '@nanostores/react'
 
 export default function Noti() {
   const pet = useStore(petStore.store)
   const [visible, setVisible] = useState(false)
+  const [need, setNeed] = useState<Field | null>(null)
 
   const isCalled = useRef(false)
 
-  const handleFullness = useCallback(
-    () => {
+  const handleNeed = useCallback(
+    ( field: Field ) => {
       if (isCalled.current) return
-      if (pet && pet.fullness <= 20) {
+      if (pet && pet[field] <= 20) {
         isCalled.current = true
+        setNeed(field)
         setVisible(true)
         setTimeout(() => setVisible(false), 5000)
       }
     }, [pet])
 
   useEffect(() => {
-    handleFullness()
-  }, [handleFullness])
+    handleNeed('fullness')
+    handleNeed('thirst')
+    handleNeed('happiness')
+  }, [handleNeed])
 
   return (
     <>
-      {visible && <div>Noti</div>}
+      {visible && need && <div>Noti</div>}
     </>
-
   )
 }
 
--- src/app/components/volume-btn.tsx
+++ src/app/components/volume-btn.tsx
@@ -1,14 +1,26 @@
 'use client'
 
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Image from 'next/image'
 
 function VolumeBtn() {
-  const [volume, setVolume] = useState(true)
+  const [volume, setVolume] = useState(false)
+
+  const saveVolumeLocally = () => {
+    setVolume(!volume)
+    localStorage.setItem('petty-volume', JSON.stringify(!volume))
+  }
+
+  useEffect(() => {
+    const savedVolume = localStorage.getItem('petty-volume')
+    if (savedVolume) {
+      setVolume(JSON.parse(savedVolume))
+    }
+  }, [])
+
   return (
     <Image className='volume--btn' src={volume ? '/buttons/volume-off.svg' : '/buttons/volume-on.svg'} alt='Volume on'
-           width={30} height={30}
-           onClick={() => setVolume(!volume)}/>
+           width={30} height={30} onClick={saveVolumeLocally}/>
   )
 }
 
