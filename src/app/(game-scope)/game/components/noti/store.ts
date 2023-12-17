9c110abaf03f94020144a29bff75b342d0184867
parlyment <parlament20000@gmail.com>
2023-12-17 17:08:38 +0200

    remove redundant type

--- src/app/(game-scope)/game/components/noti/store.ts
+++ src/app/(game-scope)/game/components/noti/store.ts
@@ -4,11 +4,11 @@ import { Field } from '@component/app/pet-store'
 import { action, atom } from 'nanostores'
 import { ReactNode } from 'react'
 
-type TimestampObject<T> = {
-  [key in T as `last_${string & key}`]: number
-}
-
 type NotiType = 'pee' | 'speak' | Field
+
+type TimestampObject = {
+  [key in NotiType]: number
+}
 
 type NotiObj = {
   type: NotiType
@@ -19,7 +19,7 @@ const store = atom<NotiObj[]>([])
 
 store.listen((value) => console.log(value, 'noti'))
 
-const timestamps: Partial<TimestampObject<NotiType>> = {}
+const timestamps: Partial<TimestampObject> = {}
 const addTimeout = 100000 //milliseconds
 
 const getNotiElement = (type: NotiType): ReactNode => {
@@ -45,11 +45,11 @@ export const notiStore = {
     if (notis.find((item) => item.type === type)) return
 
     const timestamp = new Date().getTime()
-    console.log((timestamps[`last_${type}`] ?? 0 + addTimeout) > timestamp)
-    if ((timestamps[`last_${type}`] ?? 0 + addTimeout) > timestamp) return
+    console.log((timestamps[type] ?? 0 + addTimeout) > timestamp)
+    if ((timestamps[type] ?? 0 + addTimeout) > timestamp) return
 
     const element = getNotiElement(type)
-    timestamps[`last_${type}`] = timestamp
+    timestamps[type] = timestamp
 
     store.set([...notis, { type: type, element: element }])
   }),
