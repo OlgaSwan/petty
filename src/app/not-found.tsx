da27377b4ba304d497c967a34953911fe9fabf37
OlgaSwan <137110816+OlgaSwan@users.noreply.github.com>
2023-12-18 23:34:47 +0300

    not-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import Image from 'next/image'
 
 export default function NotFound() {
   return (
-    <div>NotFound</div>
+    <div className='not--found'>
+      <h3>Sorry, page is not found</h3>
+      <Image src={'./not-found.svg'} alt='Not found' width={200} height={200}/>
+    </div>
   )
 }
